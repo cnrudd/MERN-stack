fc3aa2f93d9a8436d7ba2c828d3849d9517d1678
Colin Rudd <cnrudd@gmail.com>
2019-12-30 07:41:21 -0500

    Updated all server deps

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist";
+mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
 // Start the API server
 app.listen(PORT, function() {
